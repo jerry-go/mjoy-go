451b0df9d19ca99f2d2eb277ce89e61e28eada4b
colinandzxx <colinandzxx@163.com>
2018-05-08 17:44:59 +0800

    1. change LICENSE to GNU; 2. modify copyright;

--- src/mjoy.io/accounts/errors.go
+++ src/mjoy.io/accounts/errors.go
@@ -1,17 +1,21 @@
 ////////////////////////////////////////////////////////////////////////////////
 // Copyright (c) 2018 The mjoy-go Authors.
 //
-// The mjoy-go is free software.
+// The mjoy-go is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //
 // @File: errors.go
-// @Date: 2018/05/08 17:11:08
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//  <http://www.apache.org/licenses/>
-//
+// @Date: 2018/05/08 17:16:08
 ////////////////////////////////////////////////////////////////////////////////
 
 package accounts
