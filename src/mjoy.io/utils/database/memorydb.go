801fd2302f69d6289260401efd551513e45da755
colinandzxx <colinandzxx@163.com>
2018-05-08 17:53:47 +0800

    1. add copyright;

--- src/mjoy.io/utils/database/memorydb.go
+++ src/mjoy.io/utils/database/memorydb.go
@@ -1,11 +1,29 @@
+////////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2018 The mjoy-go Authors.
+//
+// The mjoy-go is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+// @File: memorydb.go
+// @Date: 2018/03/19 10:10:19
+////////////////////////////////////////////////////////////////////////////////
+
 package database
 
 import (
 	"sync"
 	"github.com/syndtr/goleveldb/leveldb/errors"
 )
-
-// TODO: just for test, Not yet completed, Low priority
 
 // This is a test memory database. Do not use for any production it does not get persisted
 type MemDatabase struct {
