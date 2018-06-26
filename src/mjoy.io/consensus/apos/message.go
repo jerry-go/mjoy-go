f4cb8e46ad9a1e78836e573f7e2b4b8186478947
zepple <zepple@qq.com>
2018-06-26 15:00:00 +0800

    add validate logic in message

--- src/mjoy.io/consensus/apos/message.go
+++ src/mjoy.io/consensus/apos/message.go
@@ -25,15 +25,37 @@ import (
 	"mjoy.io/common/types"
 	"mjoy.io/consensus/message"
 	"fmt"
+	"errors"
+	"hash"
+	"math/big"
 )
 //go:generate msgp
+func (cs *CredentialSign) validate() error{
+	leader := false
+	if 1 == cs.Step{
+		leader = true
+	}
+	hash := cs.hash()
+
+	//verify right
+	if isPotVerifier(hash.Bytes(), leader) == false {
+		return errors.New("credential has no right to verify")
+	}
+
+	//verify signature
+	if _, err := cs.sender(); err != nil {
+		return errors.New(fmt.Sprintf("verify CredentialSig fail: %s", err))
+	}
+
+	return nil
+}
 
 type msgCredentialSig struct {
-	cs    *CredentialSig
+	cs    *CredentialSign
 	*message.MsgPriv
 }
 
-func NewMsgCredential(c *CredentialSig) *msgCredentialSig{
+func NewMsgCredential(c *CredentialSign) *msgCredentialSig{
 	msgCs := &msgCredentialSig{
 		cs:      c,
 		MsgPriv: message.NewMsgPriv(),
@@ -42,11 +64,13 @@ func NewMsgCredential(c *CredentialSig) *msgCredentialSig{
 	return msgCs
 }
 
-func (tm *msgCredentialSig) DataHandle(data interface{}) {
+
+
+func (c *msgCredentialSig) DataHandle(data interface{}) {
 	fmt.Println("msgBlockProposal data handle")
 }
 
-func (tm *msgCredentialSig) StopHandle() {
+func (c *msgCredentialSig) StopHandle() {
 	fmt.Printf("stop ...\n")
 }
 
@@ -57,6 +81,24 @@ type BlockProposal struct {
 	Esig          *EphemeralSign
 	Credential    *CredentialSign
 }
+
+func (bp *BlockProposal) validate() error{
+	//verify Credential
+	if err := bp.Credential.validate(); err != nil {
+		return err
+	}
+
+	//verify ephemeral signature
+	bp.Esig.round = bp.Credential.Round
+	bp.Esig.step = bp.Credential.Step
+	bp.Esig.val = bp.Block.Hash().Bytes()
+	if _, err := bp.Esig.sender(); err != nil {
+		return errors.New(fmt.Sprintf("BP verify ephemeral signature fail: %s", err))
+	}
+
+	return nil
+}
+
 type msgBlockProposal struct {
 	bp    *BlockProposal
 	*message.MsgPriv
@@ -72,11 +114,12 @@ func NewMsgBlockProposal(bp *BlockProposal) *msgBlockProposal{
 	return msgBp
 }
 
-func (tm *msgBlockProposal) DataHandle(data interface{}) {
+
+func (bp *msgBlockProposal) DataHandle(data interface{}) {
 	fmt.Println("msgBlockProposal data handle")
 }
 
-func (tm *msgBlockProposal) StopHandle() {
+func (bp *msgBlockProposal) StopHandle() {
 	fmt.Printf("stop ...\n")
 }
 
@@ -90,6 +133,23 @@ type GradedConsensus struct {
 	Hash          types.Hash    //the Br's hash
 	Esig          *EphemeralSign     //the signature of somebody's ephemeral secret key
 	Credential    *CredentialSign
+}
+
+func (gc *GradedConsensus) validate() error{
+	//verify Credential
+	if err := gc.Credential.validate(); err != nil {
+		return err
+	}
+
+	//verify ephemeral signature
+	gc.Esig.round = gc.Credential.Round
+	gc.Esig.step = gc.Credential.Step
+	gc.Esig.val = gc.Hash.Bytes()
+	if _, err := gc.Esig.sender(); err != nil {
+		return errors.New(fmt.Sprintf("GC verify ephemeral signature fail: %s", err))
+	}
+
+	return nil
 }
 
 type msgGradedConsensus struct {
@@ -106,11 +166,11 @@ func NewMsgGradedConsensus(gc *GradedConsensus) *msgGradedConsensus{
 	return msgGc
 }
 
-func (tm *msgGradedConsensus) DataHandle(data interface{}) {
+func (gc *msgGradedConsensus) DataHandle(data interface{}) {
 	fmt.Println("msgGradedConsensus data handle")
 }
 
-func (tm *msgGradedConsensus) StopHandle() {
+func (gc *msgGradedConsensus) StopHandle() {
 	fmt.Printf("stop ...\n")
 }
 
@@ -124,6 +184,49 @@ type BinaryByzantineAgreement struct {
 	Hash          types.Hash
 	EsigV         *EphemeralSign
 	Credential    *CredentialSign
+}
+
+func (bba *BinaryByzantineAgreement) validate() error{
+	//verify Credential
+	if err := bba.Credential.validate(); err != nil {
+		return err
+	}
+
+	if bba.B > 1 {
+		return errors.New(fmt.Sprintf("B value %d is not right in apos protocal", bba.B))
+	}
+
+	//for step m + 3
+	if Config().maxBBASteps + 3 == int(bba.Credential.Step) {
+		// for step m +3, b must be 1 and v must be Hash(empty block(qr = last qr))
+		if bba.B != 1 {
+			logger.Info("bba m + 3 step message'b is not equal 1", bba.B)
+			return errors.New("bba m + 3 step message'b is not equal 1")
+		}
+		// todo verify empty block hash, need get right empty block
+		//if v.apos.makeEmptyBlockForTest().Hash() != msg.Hash {
+		//	logger.Info("m + 3 message hash is not empty block hash", err)
+		//	return errors.New("m + 3 message hash is not empty block hash")
+		//}
+	}
+
+	//verify B ephemeral signature
+	bba.EsigB.round = bba.Credential.Round
+	bba.EsigB.step = bba.Credential.Step
+	bba.EsigB.val = big.NewInt(int64(bba.B)).Bytes()
+	if _, err := bba.EsigB.sender(); err != nil {
+		return errors.New(fmt.Sprintf("BBA B verify ephemeral signature fail: %s", err))
+	}
+
+	//verify V ephemeral signature
+	bba.EsigV.round = bba.Credential.Round
+	bba.EsigV.step = bba.Credential.Step
+	bba.EsigV.val = bba.Hash.Bytes()
+	if _, err := bba.EsigV.sender(); err != nil {
+		return errors.New(fmt.Sprintf("BBA B verify ephemeral signature fail: %s", err))
+	}
+
+	return nil
 }
 
 type msgBinaryByzantineAgreement struct {
@@ -140,10 +243,10 @@ func NewMsgBinaryByzantineAgreement(bba *BinaryByzantineAgreement) *msgBinaryByz
 	return msgBba
 }
 
-func (tm *BinaryByzantineAgreement) DataHandle(data interface{}) {
+func (bba *BinaryByzantineAgreement) DataHandle(data interface{}) {
 	fmt.Println("BinaryByzantineAgreement data handle")
 }
 
-func (tm *BinaryByzantineAgreement) StopHandle() {
+func (bba *BinaryByzantineAgreement) StopHandle() {
 	fmt.Printf("stop ...\n")
 }
